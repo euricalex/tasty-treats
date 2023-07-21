d458334d878a89f29571e251364b059f14706d34
euricalex <122707746+euricalex@users.noreply.github.com>
2023-07-21 16:09:46 +0000

    Deploying to gh-pages from @ euricalex/tasty-treats@2db49c601439fe7efee4698b4f65f8f086715372 🚀

---
+++ index.094cafd4.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".close-recipe-svg"),o=document.querySelector(".backdrop"),c=document.querySelector(".pop-up-recipe");function n(){o.style.display="none",c.style.display="none"}console.log(e),e.addEventListener("click",n)}();
+//# sourceMappingURL=index.094cafd4.js.map
---
+++ index.0d66fc47.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".close-recipe-svg"),o=document.querySelector(".backdrop"),c=document.querySelector(".pop-up-recipe");function n(){o.style.display="none",c.style.display="none"}console.log(e),e.addEventListener("click",n);
+//# sourceMappingURL=index.0d66fc47.js.map
